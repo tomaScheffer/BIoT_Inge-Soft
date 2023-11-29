a466f4e7171f4aa57e7b87e495b2effd7c1d8dd1
LucaBloise <lbloise@itba.edu.ar>
2023-11-29 05:12:04 -0300

    Vista inicial para Home

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,18 +1,42 @@
 <script setup>
-
-    import { useAuthStore } from '@/stores/Index.js';
-
-    const authStore = useAuthStore();
-
+import { useAuthStore } from '@/stores/Index.js';
+const authStore = useAuthStore();
+let highlight = false;
 </script>
 
 <template>
-
-    <nav v-show="authStore.user" class="navbar navbar-expand navbar-dark bg-dark">
-        <div class="navbar-nav w-100 justify-content-between">
-            <router-link to="/" class="nav-item nav-link">Principal</router-link>
-            <button @click="authStore.logout()" class="btn btn-link nav-item nav-link">Cerrar Sesión</button>
-        </div>
-    </nav>
-    
+  <nav v-show="authStore.user" class="navbar navbar-expand navbar-dark" style="background-color: rgb(7, 143, 7);">
+    <div class="navbar-nav w-100 d-flex justify-content-between align-items-center">
+      <router-link to="/">
+        <img src="logo.ico" alt="Icon" class="nav-icon" />
+        <span class="nav-text" :class="{ 'highlighted': highlight }" @mouseover="highlight = true" @mouseleave="highlight = false">
+          <span class="white-text">Panel</span>
+        </span>
+      </router-link>
+      <button @click="authStore.logout()" class="btn btn-link nav-item nav-link">Cerrar Sesión</button>
+    </div>
+  </nav>
 </template>
+
+<style scoped>
+.nav-icon {
+  width: auto;
+  height: 20px;
+  margin-right: 25px; /* Increase the margin-right for more spacing */
+}
+
+/* Additional style for the text */
+.nav-text {
+  margin-top: 2px; /* Adjust the margin-top for vertical spacing */
+}
+
+/* Highlight effect */
+.highlighted {
+  text-decoration: underline;
+}
+
+/* White text style */
+.white-text {
+  color: white;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,87 @@
 <script setup>
+import { storeToRefs } from 'pinia';
+import { useAuthStore, useUsersStore } from '@/stores/Index.js';
 
-    import { storeToRefs } from 'pinia';
-    import { useAuthStore } from '@/stores/Index.js';
+const authStore = useAuthStore();
+const { user } = storeToRefs(authStore);
 
-    const authStore = useAuthStore();
-    const { user } = storeToRefs(authStore);
+const usersStore = useUsersStore();
+const { latestTemperature } = storeToRefs(usersStore);
 
+// Placeholder icons
+const temperatureIcon = "/public/thermometer.png"
+const phIcon = "/public/ph.png";
+const humidityIcon = "/public/humidity.png";
 </script>
 
 <template>
+  <div v-if="user">
+    <h1>Hola {{ user.firstName }}!</h1>
 
-    <div v-if="user">
-        <h1>Hola {{user.firstName}}!</h1>
+    <div class="sensor-cards-container">
+      <!-- Temperature Card -->
+      <div class="sensor-card">
+        <img :src="temperatureIcon" alt="Ícono de Temperatura" class="sensor-icon" />
+        <h2 class="sensor-title">Temperatura</h2>
+        <p class="latest-data">{{ latestTemperature }}°C</p>
+      </div>
+
+      <!-- pH Card -->
+      <div class="sensor-card">
+        <img :src="phIcon" alt="Ícono de pH" class="sensor-icon" />
+        <h2 class="sensor-title">Nivel de pH</h2>
+        <!-- Placeholder data, replace with actual pH data -->
+        <p class="latest-data">7.0</p>
+      </div>
+
+      <!-- Humidity Card -->
+      <div class="sensor-card">
+        <img :src="humidityIcon" alt="Ícono de Humedad" class="sensor-icon" />
+        <h2 class="sensor-title">Humedad</h2>
+        <!-- Placeholder data, replace with actual humidity data -->
+        <p class="latest-data">60%</p>
+      </div>
     </div>
-
+  </div>
 </template>
+
+<style scoped>
+.sensor-cards-container {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 20px;
+}
+
+.sensor-card {
+  width: 30%;
+  padding: 20px;
+  margin: 10px;
+  background-color: white;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.card-header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 10px;
+}
+
+.sensor-icon {
+  width: 50px;
+  height: 50px;
+  margin-bottom: 10px;
+}
+
+.sensor-title,
+.latest-data {
+  color: black;
+  margin-top: 10px;
+  white-space: nowrap; /* Prevent text from wrapping to the next line */
+}
+</style>
